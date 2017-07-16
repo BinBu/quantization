7b4465460c4148ee8a2ce3d2d232c9f8e1122ce1
Bin Hu <280891807@qq.com>
2017-07-16 22:23:41 +0800

    main program for Lloyd quantization

---
+++ main_quantization.m
@@ -0,0 +1,26 @@
+% Main program for quantization.
+
+% Written by Bin Hu
+% Date: 2017/07/16
+
+clear all
+close all
+% clc
+
+% input
+bit = 5;
+codebook = [];
+tol = eps;
+plot_flag = 1;
+
+% filename (.mat) of Lloyd quantization for the standard Gaussian
+% distribution
+lloydsfile = ['lloyds_', num2str(bit), 'bit'];
+load(lloydsfile, 'codebook'); % comment if no such file exists
+
+% Lloyd quantization for the standard Gaussian distribution
+[partition, codebook, distor, rel_distor] = mylloyds1(bit, codebook, tol, plot_flag);
+
+% save the codebook and partition in the file (.mat), or overwrite it if it
+% already exists
+save(lloydsfile,'codebook','partition');
